946aaa712b09d5aed0e7d8bd8770aa261bac751f
Mike Waldner <mike.waldner@redfall.com>
2016-05-02 13:26:56 -0600

    Initilize environment

---
+++ README.md
@@ -0,0 +1,3 @@
+Learning GO.
+
+Doing a simple Kata: http://codingdojo.org/cgi-bin/index.pl?action=browse&diff=1&id=KataGameOfLife
--- conways.go
+++ conways.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	//"strconv"
+	"strings"
 )
 
 //Conways 4 rules of life
@@ -13,27 +15,49 @@ import (
 //4. Any dead cell with exactly three live neighbours becomes a live cell.
 
 type Environment struct {
-	cells [][]int
-	x     int
-	y     int
+	cells [5][5]int
 }
 
 func main() {
-	if len(os.Args) != 4 {
+	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Not enough command line arguments\n")
 		os.Exit(1)
 	}
 
-	//x := os.Args[1]
-	//y := os.Args[2]
-	content, err := ioutil.ReadFile(os.Args[3])
+	content, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Problem with file: %v\n", err)
 		os.Exit(1)
 	}
-	//lines := strings.Split(string(content), "\n")
-	fmt.Println(string(content))
+
+	environment := initializeEnvironment(string(content))
+	fmt.Println("2d: ", environment.cells)
 }
 
-func (e *Environment) initializeCells(content) float64 {
+func initializeEnvironment(content string) Environment {
+	lines := strings.Split(content, "\n")
+	s := strings.Split(lines[0], " ")
+	if len(s) != 2 {
+		fmt.Fprintf(os.Stderr, "Problem with size format values")
+		os.Exit(1)
+	}
+
+	var cells [5][5]int
+
+	//x, _ := strconv.Atoi(s[0])
+	//y, _ := strconv.Atoi(s[1])
+	//fmt.Printf("%v %v\n", x, y)
+
+	for x, line := range lines[1:] {
+		fmt.Printf("%v\n", line)
+		for y, char := range line {
+			if char == '.' {
+				cells[x][y] = 0
+			}
+			if char == '*' {
+				cells[x][y] = 1
+			}
+		}
+	}
+	return Environment{cells}
 }
